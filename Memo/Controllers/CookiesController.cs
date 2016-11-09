36d2fd081fafd864a05e7a5d04d6241f316235fc
stu <stu@qq.com>
2016-11-09 10:47:43 +0800

    新增Cookies访问次数记录显示

---
+++ Memo/Controllers/CookiesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memo.Controllers
+{
+    public class CookiesController : Controller
+    {
+        // GET: Cookies
+        public ActionResult Index()
+        {
+            int count = 1;
+
+            var cookie = Request.Cookies["count"];
+            if(cookie == null)
+            {
+                Response.Cookies.Add(new HttpCookie("count", "1"));
+            }
+            else
+            {
+                count = int.Parse(cookie.Value) + 1;
+                var newcookie = new HttpCookie("count", count.ToString());
+                Response.Cookies.Add(newcookie);
+            }
+
+            ViewBag.count = count;
+            return View();
+        }
+    }
+}
